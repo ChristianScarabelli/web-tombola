b172206d54bb31cc99987b83e21653a547caa757
ChristianScarabelli <scachmabol@hotmail.it>
2024-12-22 11:06:04 +0100

    ADD: general layout

--- src/components/Coins/Coins.jsx
+++ src/components/Coins/Coins.jsx
@@ -1,8 +1,8 @@
 export function Coins() {
     return (
         <section className="position-relative">
-            <div className="rounded-circle" style={{ width: '80px', height: '80px', backgroundColor: 'lightgrey' }}></div>
-            <span className="display-6 bg-white rounded-circle d-flex justify-content-center align-items-center position-absolute top-0" style={{ width: '80px', height: '80px', transform: 'translate(5%, -5%)' }}> 1
+            <div className="rounded-circle" style={{ width: '70px', height: '70px', backgroundColor: 'lightgrey' }}></div>
+            <span className="display-6 bg-white rounded-circle d-flex justify-content-center align-items-center position-absolute top-0" style={{ width: '70px', height: '70px', transform: 'translate(5%, -5%)' }}> 1
             </span>
         </section>
     )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,40 +3,29 @@ import { Display } from "../Display/Display";
 
 export function Main() {
     return (
-        <>
-            <main className='py-3'>
-                <div className='container'>
-                    <div className="row">
-                        <div className='d-flex g-3 flex-wrap col-9'>
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-                            <Coins />
-
-                        </div>
-                        <div className="col-3">
-                            <Display />
+        <main className="py-3">
+            <div className="container">
+                <div className="row">
+                    {/* Colonna principale per i numeri */}
+                    <div className="col-10">
+                        <div className="row row-cols-10 g-3">
+                            {/* Generazione dinamica di 90 "Coins" */}
+                            {Array.from({ length: 90 }, (_, index) => (
+                                <div key={index} className="col">
+                                    <Coins />
+                                </div>
+                            ))}
                         </div>
                     </div>
-                </div>
-            </main>
 
-        </>
-    )
+                    {/* Colonna per il display */}
+                    <div className="col-2 d-flex flex-column align-items-center">
+                        <Display />
+                        <button className="btn btn-warning mt-3">Estrai</button>
+                        <button className="btn btn-danger mt-2">Termina Gioco</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
